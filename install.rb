8e2060a220718dbcc23fee3898620cefb4ecb950
simon.gaozheng <simon.gaozheng@gmail.com>
2016-01-18 14:06:21 +0000

    ungit the parent dir

--- install.rb
+++ install.rb
@@ -8,11 +8,12 @@ GUI_DIR = 'TypeAnnotationExercise'
 PROJECTS_ADDR = 'https://github.com/ucabgao/annotationExProjects.git'
 PROJECTS_DIR = 'annotationExProjects'
 
-if File.exist?(GIT_DIR)
-  puts 'y'
-  FileUtils.rmdir(GIT_DIR)
-end
+#if File.exist?(GIT_DIR)
+#  puts 'y'
+#  FileUtils.rmdir(GIT_DIR)
+#end
 %x{git clone #{GUI_ADDR}}
 FileUtils.cd(GUI_DIR)
+FileUtils.rmdir(GIT_DIR)
 %x{git clone #{PROJECTS_ADDR}}
 FileUtils.mv(PROJECTS_DIR, 'projects')
