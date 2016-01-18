bc174179c43c1356d4629f1c11c49408251ce647
simon.gaozheng <simon.gaozheng@gmail.com>
2016-01-18 14:03:00 +0000

    basic functions

--- install.rb
+++ install.rb
@@ -1,2 +1,18 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
+GIT_DIR = '.git'
+GUI_ADDR = 'https://github.com/ucabgao/TypeAnnotationExercise.git'
+GUI_DIR = 'TypeAnnotationExercise'
+PROJECTS_ADDR = 'https://github.com/ucabgao/annotationExProjects.git'
+PROJECTS_DIR = 'annotationExProjects'
+
+if File.exist?(GIT_DIR)
+  puts 'y'
+  FileUtils.rmdir(GIT_DIR)
+end
+%x{git clone #{GUI_ADDR}}
+FileUtils.cd(GUI_DIR)
+%x{git clone #{PROJECTS_ADDR}}
+FileUtils.mv(PROJECTS_DIR, 'projects')
